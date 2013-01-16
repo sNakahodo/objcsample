429ccb75c0b3b8bc43a3b32444510de9c9ba5c6e
sNakahodo <snakahodo55442@gmail.com>
2013-01-16 22:30:42 +0900

    Initial Commit

---
+++ rememberme/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  rememberme
+//
+//  Created by 仲程 修平 on 1/16/13.
+//  Copyright (c) 2013 仲程 修平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ remembermeTests/remembermeTests.m
@@ -0,0 +1,32 @@
+//
+//  remembermeTests.m
+//  remembermeTests
+//
+//  Created by 仲程 修平 on 1/16/13.
+//  Copyright (c) 2013 仲程 修平. All rights reserved.
+//
+
+#import "remembermeTests.h"
+
+@implementation remembermeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in remembermeTests");
+}
+
+@end
